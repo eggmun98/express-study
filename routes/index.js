3d9bfc3469e6e9f76a87d5a034cf1480d32a4075
eggmun98 <eggmun98@gmail.com>
2023-07-29 17:27:56 +0900

    [2023.07.29] comment 기본 지식 주석 추가

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
+var express = require("express"); // commonjs 방식
+// import express from "express" // module 방식
+
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// 두번째 인자에 있는 함수는 미들웨어 함수라고 한다.
+// 즉 첫번째 인자에서 엔드포인트를 적으면 그 포인트에서 두번째 인자의 함수가 실행이 된다.
+// 그 함수가 미들웨어 함수
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/qqq", (req, res) => {
+  res.send("서버를 실행시켰습니다.");
+});
+
+router.get("/qqq2", (req, res) => {
+  res.send("서버를 실행시켰습니다2.");
 });
 
 module.exports = router;
