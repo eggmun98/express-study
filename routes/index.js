a516a4ac744efbb38b594a116b2fa8dc5a81d2c5
eggmun98 <eggmun98@gmail.com>
2023-07-30 14:39:08 +0900

    [2023.07.30] stduy 기본적인 get방식 post방식의 api 기능 구현

--- routes/index.js
+++ routes/index.js
@@ -15,8 +15,37 @@ router.get("/qqq", (req, res) => {
   res.send("서버를 실행시켰습니다.");
 });
 
-router.get("/qqq2", (req, res) => {
-  res.send("서버를 실행시켰습니다2.");
+//
+
+router.get("/boards", (req, res) => {
+  // 1. db에 접속 후 데이터를 조회 즉 가져온다. => 여기서는 데이터를 조회했다고 가정할거임
+  const result = [
+    { number: 1, writer: "철수", title: "제목이에요", contents: "내용이에요" },
+    {
+      number: 2,
+      writer: "짱구",
+      title: "제목아니에요",
+      contents: "내용아니에요",
+    },
+    { number: 3, writer: "훈이", title: "제목맞아요", contents: "내용맞아요" },
+  ];
+
+  // 2. db에서 꺼내온 결과를 브라우저에 응답(response)으로 주기
+  res.send(result);
+});
+
+// 헤더에 중요한 정보들
+// 바디에 데이터들
+router.post("/boards", (req, res) => {
+  // 1. 브라우저에서 보내준 데이터 확인하기
+  console.log(req);
+  console.log("====================");
+  console.log(req.body);
+
+  // 2. db에 접속 후, 데이터를 저장 => 여기서는 데이터를 저장했다고 가정할거임
+
+  // 3. db에 저장된 결과를 브라우저에 응답(response) 주기
+  res.send("게시글 등록에 성공하였습니다.");
 });
 
 module.exports = router;
