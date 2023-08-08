a6afd35e6dced64ad15b9f1376c01f82b2a69fae
eggmun98 <eggmun98@gmail.com>
2023-08-08 13:33:21 +0900

    [2023.08.08] Feat 최신 이슈 업데이트 폴링방식 적용

--- app.js
+++ app.js
@@ -29,8 +29,7 @@ const base64Auth = authBuffer.toString("base64");
 // 지라 api 요청 설정
 let startAt = 0; // 몇번째 글부터 가져올건지 => 안적어도 됨!
 const maxResults = 200; // 한 번에 가져올 수 있는 최대 이슈 수
-const latestDate = "2023-07-01"; // 최신 날짜 // 이 부분을 유동적으로 바꿔야 함 즉 노션 db의 최신 날짜를 가져와서 바꾸기
-
+const latestDate = "2023-07-02"; // 최신 날짜
 // 지라 JQL 설정
 const jql = encodeURIComponent(
   `created >= "${latestDate}" ORDER BY created ASC`
@@ -42,7 +41,7 @@ const jql = encodeURIComponent(
 // ORDER BY created ASC 오름차순으로
 
 // 노션 마지막 ID를 가져오는 함수 => 데이터가 100개씩 가져오니 1개씩 가져오는법 찾기
-async function getLatestNotionId() {
+const getLatestNotionId = async () => {
   try {
     const response = await axios.post(
       `https://api.notion.com/v1/databases/${dbId}/query`,
@@ -62,14 +61,14 @@ async function getLatestNotionId() {
     console.error(error.message);
     throw error;
   }
-}
+};
 
-app.get("/", async (req, res) => {
+// 초기에 노션 db에 데이터가 한줄이라도 있어야 작동 => C
+const updateIssues = async () => {
   try {
-    // 1. 노션에서 최신 아이디 가져오기
-    const latestNotionId = await getLatestNotionId();
+    const latestNotionId = await getLatestNotionId(); // 노션db에 있는 가장 최신의 id값을 가져옴
 
-    // 2. 지라에서 이슈 가져오기 =>  200가 가져오도록 설정했음
+    // 지라의 이슈들을 가져옴
     const jiraResponse = await axios.get(
       `${jiraUrl}/rest/api/2/search?jql=${jql}&startAt=${startAt}&maxResults=${maxResults}`,
       {
@@ -80,6 +79,7 @@ app.get("/", async (req, res) => {
       }
     );
 
+    // 가져온 지라의 이슈들을 보기 좋게 가공함
     const issues = jiraResponse.data.issues.map((issue) => ({
       id: issue.id,
       title: issue.fields.summary,
@@ -90,12 +90,13 @@ app.get("/", async (req, res) => {
         : null,
     }));
 
-    // 3. 노션 DB의 최신 아이디와 지라의 이슈 아이디 비교
+    // 이슈 아이디를 비교해서 노션db에 있는 최신 이슈 id값보다 큰 아이디만 노션db에 추가
     for (let issue of issues) {
       if (Number(issue.id) > latestNotionId) {
         await axios.post(
           "https://api.notion.com/v1/pages",
           createPayload02(
+            // JSON 구조를 반환하는 함수
             issue.title,
             issue.state,
             issue.createDate,
@@ -113,11 +114,26 @@ app.get("/", async (req, res) => {
       }
     }
 
-    res.send("성공적으로 노션 db에 이슈를 추가하였습니다.");
+    console.log("성공적으로 노션 db에 이슈를 추가하였습니다.");
   } catch (error) {
     console.error(error);
-    res.status(500).send(error.message);
   }
+};
+
+// 반복하기 위해 재귀의 원리 사용
+const updateIssuesRepeatedly = async () => {
+  try {
+    await updateIssues(); // 업데이트 작업 기다리기
+    setTimeout(updateIssuesRepeatedly, 10000); // 업데이트가 성공적으로 완료된 후, 10초 후에 다시 호출
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+// "/" 경로에서 작동
+app.get("/", (req, res) => {
+  updateIssuesRepeatedly(); // 처음 호출
+  res.send("작동을 시작했습니다.");
 });
 
 app.listen(port, () => {
