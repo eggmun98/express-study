a1168206a7628152e07ac4ef27cc453c3a9cf544
eggmun98 <eggmun98@gmail.com>
2023-08-25 11:14:18 +0900

    [2023.08.25] Feat 선택한 하위 페이지에서도 페이지 생성 기능 구현

--- app.js
+++ app.js
@@ -1,107 +1,58 @@
-require("dotenv").config(); // 환경변수 설정 라이브러리
-const readline = require("readline");
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
-const newIssues = require("./src/newIssues");
-const editIssues = require("./src/editIssues");
+const { CLIENT_IDS } = require("./public/config");
+const rl = require("./public/utils");
+const createPage = require("./src/createPage");
+const axios = require("axios");
 
-// express에서 json 사용하려면 json()함수 사용해야 함
 app.use(express.json());
-
 require("dotenv").config();
 
-// 오류 같은거 알려주는 도구
-app.use(morgan("tiny"));
+// 유저가 터미널에서 타자를 칠수 있게 해주는 함수
+const askQuestion = (prompt) =>
+  new Promise((resolve) => rl.question(prompt, resolve));
 
-// 다른 api 개발을 위해 주석처리
-// const issuesServer = async () => {
-//   try {
-//     console.log("newIssues 함수가 실행이 되었습니다.");
-//     await newIssues();
-//     console.log("newIssues 실행이 끝났습니다.");
+// 유저가 선택한 페이지의 아이디를 저장하는 함수
+const getClientIdBySelection = async () => {
+  const selection = await askQuestion("회사를 선택하세요 (A, B, C): ");
+  const clientId = CLIENT_IDS[selection.toUpperCase()];
 
-//     console.log("editIssues 함수가 실행이 되었습니다.");
-//     await editIssues();
-//     console.log("editIssues 실행이 끝났습니다.");
-//     setTimeout(issuesServer, 60000);
-//   } catch (error) {
-//     console.error(error.message);
-//   }
-// };
-
-// issuesServer(); // 서버 시작시 자동으로 실행됨
-
-// 현재 문제점 이슈 업데이트는 최대 100개 검사밖에 못해서 한계가 있음 => 개선하려면 노션db 가져오는거 추가로 요청해야 함
-// => 근데 프로젝트별로 이슈를 나눌거니까 괜찮을거 같음
-
-// 터미널 명령어 입력을 위한 readline 인터페이스 생성
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-const apiKey = process.env.NOTION_API_KEY;
-const pageId = process.env.NOTION_PAGE_ID04;
-
-const NOTION_API_URL = "https://api.notion.com/v1/pages";
-const HEADERS = {
-  Authorization: `Bearer ${apiKey}`,
-  "Content-Type": "application/json",
-  "Notion-Version": "2022-06-28",
-};
-
-const payload = (page_id, title) => {
-  return {
-    parent: { page_id },
-    properties: {
-      title: {
-        title: [{ text: { content: title } }],
-      },
-    },
-  };
-};
-
-async function createPage(page_id, title) {
-  try {
-    const response = await axios.post(NOTION_API_URL, payload(page_id, title), {
-      headers: HEADERS,
-    });
-
-    return response.data.id;
-  } catch (error) {
-    throw new Error(error.message);
+  if (!clientId) {
+    console.error("잘못된 선택입니다.");
+    rl.close();
+    process.exit();
   }
-}
 
-const askQuestion = (prompt) => {
-  return new Promise((resolve) => {
-    rl.question(prompt, resolve);
-  });
+  return clientId;
 };
 
-app.listen(PORT, async () => {
-  console.log(
-    "1. 자식페이지 이름을 적으세요.(자식 페이지 이름을 다 적었으면 손자 페이지 이름도 입력하세요.)"
+const getLowIdBySelection = async () => {
+  const page = await notionPageId();
+
+  const lowPageList = page.map((el, dex) => " " + String(dex) + "." + el.title);
+  // 하위 페이지들의 번호와 리스트들을 보여준다.
+  const selection = await askQuestion(
+    `하위 페이지의 번호를 선택하세요 ${lowPageList} `
   );
-
-  try {
-    const childTitle = await askQuestion("자식 페이지의 이름을 입력하세요: ");
-    const childPageId = await createPage(pageId, childTitle);
-    console.log(`자식 페이지 ID: ${childPageId}`);
-
-    const grandchildTitle = await askQuestion(
-      "손자 페이지의 이름을 입력하세요: "
-    );
-    const grandchildPageId = await createPage(childPageId, grandchildTitle);
-    console.log(`손자 페이지 ID: ${grandchildPageId}`);
-
-    rl.close();
-  } catch (error) {
-    console.error("Error:", error);
-    rl.close();
+  // 유저가 선택한 페이지 번호의 아이디를 저장한다.
+  if (selection) {
+    const clientId = page
+      .map((el) => el.id)
+      .filter((_, dex) => dex === Number(selection));
+    return clientId[0];
   }
-});
+
+  if (!clientId) {
+    // 이 코드 수정하기
+    console.error("잘못된 선택입니다.");
+    rl.close();
+    process.exit();
+  }
+};
+
+async function pageLevelPick() {
+  return await askQuestion("1: 상위 페이지, 2: 하위 페이지 ");
+}
 
 const notionPageId = async () => {
   try {
@@ -114,12 +65,56 @@ const notionPageId = async () => {
         },
       }
     );
-    // 페이지 아이디들만 추출
-    const pageIds = response.data.results.map((el) => el);
-    console.log(pageIds);
+    const page = response.data.results.map((el) => {
+      return {
+        id: el.id,
+        title: el.child_page.title,
+      };
+    });
+    return page;
   } catch (error) {
     console.log(error.message);
   }
 };
+
+const notionServer = async () => {
+  console.log("서버가 실행이되었습니다.");
+  try {
+    // 1. 최상위 페이지들을 선택하게한다 . => 유저가 선택한 페이지의 아이디를 저장한다.
+    let clientId = await getClientIdBySelection();
+
+    // 2. 최상위 페이지에서 생성할 지 아니면 하위 페이지에서 생성할 지 선택하게 한다.
+    const userPick = await pageLevelPick();
+
+    if (userPick === "2") {
+      // 1. 하위 페이지의 리스트들을 보여준다.
+      // 선택한 페이지의 아이디를 다시 clientId에 재할당
+      clientId = await getLowIdBySelection();
+    }
+
+    // 3. 자식 페이지의 이름을 설정한다.
+    const childTitle = await askQuestion("자식 페이지의 이름을 입력하세요: ");
+
+    // 4. 자식 페이지를 생성한다.
+    const childPageId = await createPage(clientId, childTitle);
+    console.log(`자식 페이지 ID: ${childPageId}`);
+
+    // 5. 손자 페이지의 이름을 설정한다.
+    const grandchildTitle = await askQuestion(
+      "손자 페이지의 이름을 입력하세요: "
+    );
+
+    // 6. 손자 페이지를 생성한다.
+    const grandchildPageId = await createPage(childPageId, grandchildTitle);
+    console.log(`손자 페이지 ID: ${grandchildPageId}`);
+    rl.close();
+  } catch (error) {
+    console.error(error.message);
+    rl.close();
+  }
+};
+
+// notionPageId();
+notionServer();
 
 module.exports = app;
